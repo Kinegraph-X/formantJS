d88cc623db811e2596bf7e7ee09356828ee2b16f
sylvain <vassilyB@gmail.com>
2024-05-11 01:38:14 +0200

    Updated install.bat : the browserify-bugfix isn't called by npm

--- install.bat
+++ install.bat
@@ -5,4 +5,6 @@ npm -g install grunt ^
 & curl -L https://github.com/Kinegraph-X/FormantBundler/releases/download/0.0.1/FormantBundler.zip -o file.zip ^
 & 7z x file.zip ^
 & del file.zip ^
-& move /Y package.json ..\.
+& move /Y package.json ..\. ^
+&cd .. ^
+& node node_modules\formantjs\browserify-bugfix.js

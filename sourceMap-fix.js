1009d793f306e63dc0032748fc861b5a929b3f80
sylvain <vassilyB@gmail.com>
2024-05-11 19:45:24 +0200

    Added sourceMap "hot replacement" of absolute path to core sourceMaps

--- sourceMap-fix.js
+++ sourceMap-fix.js
@@ -1,24 +1,43 @@
 const fs = require('fs');
 
 const rootPath = 'node_modules/formantjs';
-const filePath = rootPath + '/formant.js.map';
-const searchString = /("sourceRoot":\s")(.*?)(")/;
+const coreFilePath = rootPath + '/formant.js.map';
+const componentLibFilePath = rootPath + '/formant.js.map';
+const coreSearchString = '_formantCoreBundler-master/build';
+const componentLibSearchString = /("sourceRoot":\s")(.*?)(")/;
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(coreFilePath, 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading file:', err);
     return;
   }
 
-  const modifiedData = data.replace(searchString, function(match, c1, c2, c3) {
-	return c1 + '../..' + c3
-  });
+  const modifiedData = data.replace(coreSearchString, 'node_modules/formantjs');
 
-  fs.writeFile(filePath, modifiedData, 'utf8', (err) => {
+  fs.writeFile(coreFilePath, modifiedData, 'utf8', (err) => {
     if (err) {
       console.error('Error writing file:', err);
       return;
     }
-    console.log('SourceMaps have been patched successfully.');
+    console.log('Core SourceMaps have been patched successfully.');
+  });
+});
+
+fs.readFile(componentLibFilePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+
+  const modifiedData = data.replace(componentLibSearchString, function(match, c1, c2, c3) {
+	return c1 + '../..' + c3
+  });
+
+  fs.writeFile(componentLibFilePath, modifiedData, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return;
+    }
+    console.log('ComponentLib SourceMaps have been patched successfully.');
   });
 });

71f3e7528340b5369bbdfccb873a0e7c2974c469
sylvain <vassilyB@gmail.com>
2024-05-11 20:21:03 +0200

    Fixed sourceMap-fix.js: using 2 functions was a complete mess

--- sourceMap-fix.js
+++ sourceMap-fix.js
@@ -1,43 +1,26 @@
 const fs = require('fs');
 
 const rootPath = 'node_modules/formantjs';
-const coreFilePath = rootPath + '/formant.js.map';
-const componentLibFilePath = rootPath + '/formant.js.map';
-const coreSearchString = '_formantCoreBundler-master/build';
+const filePath = rootPath + '/formant.js.map';
+const coreSearchString = /_formantCoreBundler-master\/build/;
 const componentLibSearchString = /("sourceRoot":\s")(.*?)(")/;
 
-fs.readFile(coreFilePath, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading file:', err);
     return;
   }
 
-  const modifiedData = data.replace(coreSearchString, 'node_modules/formantjs');
-
-  fs.writeFile(coreFilePath, modifiedData, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing file:', err);
-      return;
-    }
-    console.log('Core SourceMaps have been patched successfully.');
-  });
-});
-
-fs.readFile(componentLibFilePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
-
-  const modifiedData = data.replace(componentLibSearchString, function(match, c1, c2, c3) {
+	let modifiedData = data.replace(coreSearchString, 'node_modules/formantjs');
+	modifiedData = modifiedData.replace(componentLibSearchString, function(match, c1, c2, c3) {
 	return c1 + '../..' + c3
   });
-
-  fs.writeFile(componentLibFilePath, modifiedData, 'utf8', (err) => {
+  
+  fs.writeFile(filePath, modifiedData, 'utf8', (err) => {
     if (err) {
       console.error('Error writing file:', err);
       return;
     }
-    console.log('ComponentLib SourceMaps have been patched successfully.');
+    console.log('SourceMap has been patched successfully.');
   });
 });
